b6c744e1f68c896a6d368f320f6a64fa5fd4c879
bperezgo <bsperezgo@gmail.com>
2022-01-19 20:35:02 -0500

    feat: initial setup of the prohect

---
+++ next.config.js
@@ -0,0 +1,7 @@
+const config = {
+  future: {
+    webpack5: true,
+  },
+};
+
+module.exports = config;
---
+++ pages/index.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+  return <div>
+    <div>Profile</div>
+    <div>Input</div>
+  </div>;
+}
