290f973d837dac06acfb78418caf80cd5a2429d6
bperezgo <bsperezgo@gmail.com>
2022-01-21 00:03:13 -0500

    feat: profile img changed

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -9,8 +9,10 @@ export const Profile = ({ img }: ProfileProps) => (
     <div className={styles.external__circle}>
       <div
         className={styles.inner__circle}
-        style={{ background: `url('${img}') no-repeat center` }}
-      ></div>
+        // style={{ background: `url('${img}') no-repeat none` }}
+      >
+        <img src={img} alt="" />
+      </div>
     </div>
   </div>
 )
