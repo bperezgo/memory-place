a01a54b9c969ac932712c6f753e88beea6858cd7
bperezgo <bsperezgo@gmail.com>
2022-01-20 19:01:30 -0500

    feat: Login design finish

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,9 +1,16 @@
 import styles from './Profile.module.scss'
 
-export const Profile = () => (
+type ProfileProps = {
+  img: string
+}
+
+export const Profile = ({ img }: ProfileProps) => (
   <div className={styles.ProfileContainer}>
     <div className={styles.external__circle}>
-      <div className={styles.inner__circle}></div>
+      <div
+        className={styles.inner__circle}
+        style={{ background: `url('${img}') no-repeat center` }}
+      ></div>
     </div>
   </div>
 )
--- pages/Login.tsx
+++ pages/Login.tsx
@@ -2,11 +2,14 @@ import { Profile } from '@components/Profile'
 import { InputSecretPassword } from '@components/InputSecretPassword'
 import styles from './login.module.scss'
 
+const exampleImg =
+  'https://lh3.googleusercontent.com/HThMSkbSg2ntSQZdcFsOtflgkL6vBJPm6bJFeat2c64WkMjvLeHjEvux2U8fFOHydrNz--kcAVv4AQMlD4Z9sR60ZA=w640-h400-e365-rj-sc0x00ffffff'
+
 export default function Login() {
   return (
     <div className={styles.LoginContainer}>
       <div className={styles.Login__box}>
-        <Profile />
+        <Profile img={exampleImg} />
         <InputSecretPassword />
       </div>
     </div>
