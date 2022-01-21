47bf380cfb3d1e2a4e6ea29b133afe463ffd503f
bperezgo <bsperezgo@gmail.com>
2022-01-20 23:59:18 -0500

    feat: portrait works

--- components/Portrait.tsx
+++ components/Portrait.tsx
@@ -3,20 +3,26 @@ import styles from './Portrait.module.scss'
 
 type PortraitProps = {
   img: string
+  onNextImg: () => void
+  onPrevImg: () => void
 }
 
-export const Portrait = ({ img }: PortraitProps) => {
+export const Portrait = ({ img, onNextImg, onPrevImg }: PortraitProps) => {
   return (
     <div className={styles.ExternalPortrait}>
-      <div className={`${styles.PrevButton} ${styles.PortraitButton}`}>
+      <div
+        className={`${styles.PrevButton} ${styles.PortraitButton}`}
+        onClick={onNextImg}
+      >
         <BiCaretLeft />
       </div>
-      <div
-        className={styles.InternalPortrait}
-      >
-          <img src={img} alt="" />
+      <div className={styles.InternalPortrait}>
+        <img src={img} alt="" />
       </div>
-      <div className={`${styles.NextButton} ${styles.PortraitButton}`}>
+      <div
+        className={`${styles.NextButton} ${styles.PortraitButton}`}
+        onClick={onPrevImg}
+      >
         <BiCaretRight />
       </div>
     </div>
--- pages/Portraits.tsx
+++ pages/Portraits.tsx
@@ -2,6 +2,7 @@ import { GetStaticProps, InferGetStaticPropsType } from 'next'
 import { Portrait } from '@components/Portrait'
 import { IMemory } from '@interfaces/memories'
 import { getMemories } from '@api/memories'
+import { useState } from 'react'
 
 type MemoriesProps = {
   memories: IMemory[]
@@ -19,10 +20,27 @@ export default function Portraits({
   memories,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   const totalImgs = memories.length
+  const [currentMemoryIdx, setCurrentMemoryIdx] = useState(0)
+  const onNextImg = () => {
+    if (currentMemoryIdx >= totalImgs - 1) {
+      return setCurrentMemoryIdx(0)
+    }
+    setCurrentMemoryIdx(currentMemoryIdx + 1)
+  }
+  const onPrevImg = () => {
+    if (currentMemoryIdx <= 0) {
+      return setCurrentMemoryIdx(totalImgs - 1)
+    }
+    setCurrentMemoryIdx(currentMemoryIdx - 1)
+  }
   // TODO: If memories comes empty, create another logic
   return (
     <div className="CenteredContainer CenteredVerticalAlign">
-      <Portrait img={memories[0].img} />
+      <Portrait
+        img={memories[currentMemoryIdx].img}
+        onNextImg={onNextImg}
+        onPrevImg={onPrevImg}
+      />
     </div>
   )
 }
