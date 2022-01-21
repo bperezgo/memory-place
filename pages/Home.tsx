dc1374e39368d4241ef1b8023a12804a9bcbaa07
bperezgo <bsperezgo@gmail.com>
2022-01-20 23:14:30 -0500

    feat: setTimeout to change to the memories

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,8 +1,16 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import styles from './home.module.scss'
 
 export default function Home() {
+  const router = useRouter()
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/portraits')
+    }, 3000)
+  }, [])
   return (
-    <div className='CenteredContainer CenteredTextAlign CenteredVerticalAlign'>
+    <div className="CenteredContainer CenteredTextAlign CenteredVerticalAlign">
       <h1 className={styles.Title}>Un lugar para guardar nuestros recuerdos</h1>
     </div>
   )
